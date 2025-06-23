1ba1ba71f23e8573e85e22f48baac4297ea817e2
Tayakon Intanon <tachagron1964@gmail.com>
2025-06-23 10:21:07 +0700

    s

---
+++ src/app/about/page.js
@@ -0,0 +1,9 @@
+export default function About() {
+  return (
+    <>
+   <h1 className="text-4xl text-center text-blue-500"
+   > About Page
+    </h1>
+    </>
+  );
+}
---
+++ src/app/page.tsx
@@ -0,0 +1,8 @@
+
+export default function Home() {
+  return (
+    <>
+   <h1 className="text-4xl text-center text-green-500"> Home Page</h1>
+    </>
+  );
+}
